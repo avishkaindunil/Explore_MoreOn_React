54539fb74a8faff3834ea9347e23deae00aeb047
ai7567 <avishka.indunil@zebra.com>
2025-02-03 16:44:57 +0530

    URL parameters set

--- blog-app-backend/src/server.js
+++ blog-app-backend/src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 
 const app = express ();
+app.use(express.json());
 
 app.get('/hello', (req, res) => {
-    res.send('Hello!');
+    res.send(`Hello ${req.body.name}!`);
 })
+
+app.get('/hello/:name/goodbye/:otherName', (req,res) => {
+    console.log(req.params);
+    const { name } = req.params;
+    res.send(`Hello ${name}!!`);
+});
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
