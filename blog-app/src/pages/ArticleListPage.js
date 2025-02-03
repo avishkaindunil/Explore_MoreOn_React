a2eb04fe99bfadadb56f08540a2004113445cfb8
ai7567 <avishka.indunil@zebra.com>
2025-02-03 14:39:59 +0530

    articles linked

--- blog-app/src/pages/ArticleListPage.js
+++ blog-app/src/pages/ArticleListPage.js
@@ -1,6 +1,18 @@
+import articles from "./article-content";
+import { Link } from 'react-router-dom';
+
 const ArticleListPage = () => {
     return (
-        <h1>This is the Article List page!</h1>
+        <>
+            <h1>Articles</h1>
+            {articles.map(article => (
+                <Link key={article.name} className='article-list-item' to={`/articles/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0,200)}...</p>
+                </Link>
+            ))}
+        </>
+        
     );
 }
 
