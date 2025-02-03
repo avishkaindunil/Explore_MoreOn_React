6cecfe8f217185c274eeba414689201cb6a88b4e
ai7567 <avishka.indunil@zebra.com>
2025-02-03 15:26:54 +0530

    error 404 page created

--- blog-app/src/pages/NotFoundPage.js
+++ blog-app/src/pages/NotFoundPage.js
@@ -1,7 +1,5 @@
-const NotFoundPage = () => {
-    return (
-        <h1>Oops! Error 404</h1>
-    );
-}
+const NotFoundPage = () => (
+        <h1>Oops! Error 404: Page Not Found</h1>
+);
 
 export default NotFoundPage;
