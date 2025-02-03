af229440c2fee630fc05b49c7898e8d2c24460e3
ai7567 <avishka.indunil@zebra.com>
2025-02-03 19:23:00 +0530

    comments added

--- blog-app-backend/src/server.js
+++ blog-app-backend/src/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express ();
@@ -24,6 +27,20 @@ app.put('/api/articles/:name/upvote', (req, res) => {
      } else {
         res.send('That article doesn\'t exist');
      }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
 });
 
 // app.get('/hello', (req, res) => {
