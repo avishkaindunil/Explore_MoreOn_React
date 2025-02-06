0c363e5987961deb6df4f9e5f1b59f281fa1bc0e
ai7567 <avishka.indunil@zebra.com>
2025-02-06 15:27:20 +0530

    upvote endpoint updated

--- blog-app-backend/src/server.js
+++ blog-app-backend/src/server.js
@@ -35,16 +35,27 @@ app.get('/api/articles/:name', async (req, res) => {
     }
 });
 
-app.put('/api/articles/:name/upvote', (req, res) => {
-     const { name } = req.params;
-     const article = articlesInfo.find(a => a.name === name);
+app.put('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params;
+    //  const article = articlesInfo.find(a => a.name === name);
      
-     if (article) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+    await db.collection('articles').updateOne({ name }, {
+        $inc: { upvotes: 1 },
+        // $set: { upvotes: 100 },
+    });
+
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article) {
         article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
-     } else {
+    } else {
         res.send('That article doesn\'t exist');
-     }
+    }
 });
 
 app.post('/api/articles/:name/comments', (req, res) => {
