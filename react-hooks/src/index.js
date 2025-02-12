26883553637338f5ad0a1a84877349532e8a5908
ai7567 <avishka.indunil@zebra.com>
2025-02-12 17:58:42 +0530

    incorporating the useState Hook

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function App() {
+  const [status, setStatus] = useState("Not Deleivered");
+  return(
+    <div>
+      <h1>The package is: {status}</h1>
+      <button onClick={() => setStatus("Delivered")}>Deliver</button>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App name="Alex"/>
+    <App />
   </React.StrictMode>
 );
 
