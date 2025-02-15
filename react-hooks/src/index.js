ba4f16ae55162221dc92e54d09292539bd013478
ai7567 <avishka.indunil@zebra.com>
2025-02-15 18:36:05 +0530

    created a simple dependency array

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -6,15 +6,29 @@ import { FaStar } from "react-icons/fa";
 
 function App() {
   const [name, setName] = useState("Jan");
+  const [admin, setAdmin] = useState(false);
 
   useEffect(() => {
+    console.log(`Celebrate ${name}`);
     document.title = `Celebrate ${name}`;
-  });
+  }, [name]);
+
+  useEffect(() => {
+    console.log(
+      `The user is: ${
+        admin ? "admin" : "not admin"
+      }.`
+    );
+  }, [admin]);
 
   return (
     <section>
       <p>Congratulations {name}!</p>
       <button onClick={() => setName("Will")}>Change Winner</button>
+      <p>
+        {admin ? "logged in" : "not logged in"}
+      </p>
+      <button onClick={() => setAdmin(true)}>Log In</button>
     </section>
   );
 }
